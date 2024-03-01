65c1e1d41567920e2bb46ed27ef02c6b8bd776bc
Seyamack Afzali <seyamack@gmail.com>
2024-02-29 22:13:13 -0600

    pre-commit works

--- game.py
+++ game.py
@@ -1,33 +1,21 @@
 import pygame
 
-
-
-
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
-if __name__ == '__main__':
+if __name__ == "__main__":
     while running:
-        
-
         for event in pygame.event.get():
-            #breakpoint()
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill('black')
+        screen.fill("black")
 
         # TODO
-
-
-
-
 
         pygame.display.flip()
 
         clock.tick(60)
 
     pygame.quit()
-
-    
